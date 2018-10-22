501f74dfdd629fef65593948393a8c5dc1d3cac8
apple <734719526@qq.com>
2018-10-22 15:00:16 +0800

    项目初始化1

---
+++ Assets/Scrpit/base/BaseMVC.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public abstract class BaseMVC 
+{
+    //上下文对象
+    public BaseMonoBehaviour mContent;
+
+    /// <summary>
+    /// 初始化数据
+    /// </summary>
+    public abstract void initData();
+
+    /// <summary>
+    /// 设置上下文对象
+    /// </summary>
+    /// <param name="content"></param>
+    public void setContent(BaseMonoBehaviour content)
+    {
+        this.mContent = content;
+    }
+
+    /// <summary>
+    /// 获取上下文对象
+    /// </summary>
+    /// <returns></returns>
+    public BaseMonoBehaviour getContent()
+    {
+        return mContent;
+    }
+}
---
+++ Assets/Scrpit/base/BaseMVCController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public abstract  class BaseMVCController<M,V> : BaseMVC
+{
+    //模型
+    public M mModel;
+    //视图
+    public V mView;
+
+    /// <summary>
+    /// 设置模型
+    /// </summary>
+    /// <param name="model"></param>
+    public void setModel(M model)
+    {
+        this.mModel= model;
+    }
+
+    /// <summary>
+    /// 设置视图
+    /// </summary>
+    /// <param name="mView"></param>
+    public void setView(V view)
+    {
+        this.mView = view;
+    }
+
+
+}
